34f3da01054dc3162dc7f841e73f71204c0659c6
Gabriel Pires <gabrielhpires@gmail.com>
2026-09-08 00:07:22 -0300

    feat: add new endpoint to get all trips from a user

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,11 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from apps.trips.models import Trip
+from apps.trips.serializers import TripSerializer
 
 from .models import User
 from .serializers import UserSerializer
@@ -16,3 +21,21 @@ class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     renderer_classes = [JSONRenderer]
+
+
+@extend_schema(
+    tags=["Users"],
+    summary="List user trips",
+    description="Returns all trips from a user.",
+    responses={200: TripSerializer(many=True)},
+)
+class UserTripList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    serializer_class = TripSerializer
+    renderer_classes = [JSONRenderer]
+
+    def get_queryset(self):
+        user_id = self.request.user
+        return Trip.objects.filter(user=user_id).select_related("user")
